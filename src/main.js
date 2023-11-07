31b1c9d41e03325612bfb448250e3c8cc97ed5f2
AichKyo <754555298@qq.com>
2023-11-08 02:51:57 +0800

    feat：引入组件

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import hroaUI from "hroa-ui";
 
+// 全局引入UI组件
+import hroaUI from "hroa-ui";
+// 引入样式
+import 'hroa-ui/dist/hrui.css';
+// 组册
 Vue.use(hroaUI)
+
+
 Vue.config.productionTip = false
 
 new Vue({
